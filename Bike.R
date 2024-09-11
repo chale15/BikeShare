b2e566a18b58f50a8463a3f3985cf6478a89c6a4
Carter Hale <chale15@byu.edu>
2024-09-11 08:56:48 -0600

    EDA on Bike.R file, add data

--- Bike.R
+++ Bike.R
@@ -3,3 +3,29 @@ library(tidymodels)
 library(vroom)
 library(DataExplorer)
 library(patchwork)
+library(GGally)
+
+train <- vroom("~/Desktop/Fall 2024/Stat 348/GitHubRepos/BikeShare/train.csv")
+train$season <- as.factor(train$season)
+levels(train$season) <- c("Spring", "Summer", "Fall", "Winter")
+
+
+weather_bar <- ggplot(data = train, mapping = aes(x = weather)) + 
+  geom_bar(fill = "deepskyblue") + 
+  labs(title = "Weather Bar Plot",x = "Type of Weather", y="Count")
+
+temp_count <- ggplot(data = train, mapping = aes(x = temp, y = count)) +
+  geom_point(size=1, colour = "deepskyblue4", alpha = 0.8) + geom_smooth(se=FALSE, color = "red", lwd = 1) +
+  labs(title = "Bike Rentals by Temperature", x = "Temperature (Celsius)", y = "Count")
+
+count_over_time <- ggplot(data = train, mapping = aes(x = datetime, y = count)) +
+  geom_point(size=1, colour = "forestgreen", alpha = 0.6) + geom_smooth(se=FALSE, color = "red", lwd = 1) +
+  labs(title = "Bike Rentals over Time", x = "Date (Year-Month)", y = "Count")
+
+count_by_season <- ggplot(data = train, mapping = aes(x=season, y = count)) + 
+  geom_boxplot(fill = "darkgreen", alpha = .7) + 
+  labs(title = "Bike Rental by Season", x = "Season", y = "Count")
+
+four_plot <- (weather_bar + temp_count)/(count_over_time + count_by_season)
+
+ggsave("four_plots.jpg", plot = four_plot, path = "~/Desktop/Fall 2024/Stat 348/GitHubRepos/BikeShare/")
