f380bec12a9e8bffd047e38a749fa302e52e09dd
Carter Hale <chale15@byu.edu>
2024-09-11 09:50:42 -0600

    Add Linear Model Predictions

--- Bike.R
+++ Bike.R
@@ -5,11 +5,16 @@ library(DataExplorer)
 library(patchwork)
 library(GGally)
 
+#Import Training and Testing Data
 train <- vroom("~/Desktop/Fall 2024/Stat 348/GitHubRepos/BikeShare/train.csv")
-train$season <- as.factor(train$season)
-levels(train$season) <- c("Spring", "Summer", "Fall", "Winter")
+test <- vroom("~/Desktop/Fall 2024/Stat 348/GitHubRepos/BikeShare/test.csv")
+
+#Refactor
+train$season <- factor(train$season, levels = c("Spring", "Summer", "Fall", "Winter"))
+train$weather <- factor(train$weather, levels = c("Sunny", "Cloudy", "Light Rain", "Heavy Rain"))
 
 
+#EDA Plots
 weather_bar <- ggplot(data = train, mapping = aes(x = weather)) + 
   geom_bar(fill = "deepskyblue") + 
   labs(title = "Weather Bar Plot",x = "Type of Weather", y="Count")
@@ -29,3 +34,6 @@ count_by_season <- ggplot(data = train, mapping = aes(x=season, y = count)) +
 four_plot <- (weather_bar + temp_count)/(count_over_time + count_by_season)
 
 ggsave("four_plots.jpg", plot = four_plot, path = "~/Desktop/Fall 2024/Stat 348/GitHubRepos/BikeShare/")
+
+#Linear Regression
+
